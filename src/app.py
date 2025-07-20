0bbe482c8a7c50c48b8a05b65f3ce95f850e3d15
pcaliman <pcaliman@hotmail.com>
2025-07-20 00:02:15 -0400

    Update app.py

--- src/app.py
+++ src/app.py
@@ -40,9 +40,61 @@ def autocomplete_estados():
 @app.route("/", methods = ["GET", "POST"])
 def index():
 
+
     pred_class = None
     if request.method == "POST":
-        y_pred=1234
+        
+        modelo = request.form.get("modelo", "")
+
+        yearr = float(request.form["yearr"])
+        odometerr = float(request.form["Odometerr"])
+        fabricante = request.form["fabricante"]
+        modelo = request.form["modelo"]
+        condittionn = request.form["condittionn"]
+        fuell = request.form["fuell"]
+        tituloo = request.form["Tituloo"]
+        caja = request.form["Caja"]
+        statee = request.form["statee"]
+        fechaa = request.form["fechaa"]
+
+        # Puedes imprimir para verificar que llegan bien:
+        #print(">>> Datos recibidos:")
+        #print("Año:", yearr)
+        #print("Odómetro:", odometerr)
+        #print("Fabricante:", fabricante)
+        #print("Modelo:", modelo)
+        #print("Condición:", condittionn)
+        #print("Combustible:", fuell)
+        #print("Título:", tituloo)
+        #print("Caja:", caja)
+        #print("Estado:", statee)
+        #print("Fecha:", fechaa)
+
+
+
+        data = [[yearr, odometerr, fabricante, modelo, condittionn, fuell, tituloo, caja, statee, fechaa]]
+
+        fecha_cruda = request.form.get("fechaa")
+        fecha_formateada = formatear_fecha(fecha_cruda)
+
+
+        columnas = ['year', 'manufacturer', 'model', 'condition', 'fuel', 'odometer', 'title_status', 'transmission', 'state', 'posting_date']
+        dato_crudo = [yearr, fabricante, modelo, condittionn, fuell, odometerr, tituloo, caja, statee, fecha_formateada]
+#dato_crudo = [2006, 'ram', '2500', 'good', 'gas', 129761, 'clean', 'automatic', 'in', '2021-04-29T16:03:59-0400']
+
+
+        df_test = pd.DataFrame([dato_crudo], columns=columnas)
+        #print(df_test.head())
+
+        X_test_processed = preprocessor.transform(df_test)
+        X_test_scaled = scaler.transform(X_test_processed)
+        y_pred = modelopredictor.predict(X_test_scaled)
+
+
+        #print(data)
+       # prediction = str(model.predict(data)[0])
+       # pred_class = class_dict[prediction]
+
         pred_class=int(y_pred)
     else:
         pred_class = None
